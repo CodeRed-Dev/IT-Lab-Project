084287f8ccfab911ef0ca0babbf0c6ae48d5506b
Raghav Sharma <116653315+CodeRed-Dev@users.noreply.github.com>
2023-04-28 09:12:53 +0530

    Add files via upload

--- newsapp/newsapp/urls.py
+++ newsapp/newsapp/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-
+from reader import views
 urlpatterns = [
+    #path('login/', views.loginPage, name="login"),
     path('admin/', admin.site.urls),
     path('', include("reader.urls")),
 ]
